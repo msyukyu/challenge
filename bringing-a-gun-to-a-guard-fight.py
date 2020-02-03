1e50eccd8824507c4398c141d5881ad839157ad9
msyukyu <damien_ab@hotmail.com>
2020-02-03 15:36:47 +0100

    alpha bring a gun

--- bringing-a-gun-to-a-guard-fight.py
+++ bringing-a-gun-to-a-guard-fight.py
@@ -1,13 +1,9 @@
+from math import sqrt
+
 class Coordinates:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
-
-	def __equ__(self, coordinates):
-		return coordinates.x == self.x and coordinates.y == self.y
-
-	def __hash__(self):
-		return self.x * 1251 + self.y
 
 	def mult(self, c):
 		self.x *= c
@@ -39,33 +35,63 @@ class Coordinates:
 		origin.c_mult(axis).sub(self.clone().c_mult(axis)).mult(2)
 		return self.add(origin.c_mult(axis))
 
+	def distance(self, coordinates):
+		a = self.x - coordinates.x
+		b = self.y - coordinates.y
+		return sqrt(a * a + b * b)
+
 class Room:
-	orientations = {
-		Coordinates(0, 1): [Coordinates(0, 1), Coordinates(-1, 0)],
-		Coordinates(-1, 0): [Coordinates(-1, 0), Coordinates(0, -1)],
-		Coordinates(0, -1): [Coordinates(0, -1), Coordinates(1, 0)],
-		Coordinates(1, 0): [Coordinates(1, 0), Coordinates(0, 1)]
+	center = Coordinates(0, 0)
+	axis = {
+		"N": Coordinates(0, 1),
+		"S": Coordinates(0, -1),
+		"W": Coordinates(-1, 0),
+		"E": Coordinates(1, 0)
 	}
-	def __init__(self, origin, dimensions, host, target, clones):
+	def __init__(self, origin, dimensions, host, target, mirrors):
 		self.dimensions = dimensions
 		self.host = host
 		self.target = target
 		self.origin = origin
-		self.clones = clones
+		self.mirrors = mirrors
 
 	def clone(self, list_rooms):
-		for clone in self.clones:
-			origin = self.dimensions.clone().c_mult(clone).add(self.origin)
+		for orientation in self.mirrors:
+			axis = Room.axis[orientation]
+			origin = self.dimensions.clone().c_mult(axis).add(self.origin)
 			dimensions = self.dimensions
-			host = self.host.clone().mirror(origin, dimensions, clone)
-			target = self.target.clone().mirror(origin, dimensions, clone)
-			clones = Room.orientations[clone]
-			list_rooms.append(Room(origin, dimensions, host, target, clones))
+			host = self.host.clone().mirror(origin, dimensions, axis)
+			target = self.target.clone().mirror(origin, dimensions, axis)
+			mirrors = self.get_mirrors(orientation)
+			list_rooms.append(Room(origin, dimensions, host, target, mirrors))
+	
+	def get_mirrors(self, orientation):
+		mirrors = []
+		if orientation == "W" or orientation == "E":
+			mirrors.append(orientation)
+		else:
+			mirrors.append(orientation)
+			mirrors.append("W")
+			mirrors.append("E")
+		return mirrors
 
 def solution(dimensions, your_position, guard_position, distance):
 	C = Coordinates
-	init = Room(C(0, 0),
+	host = C(your_position[0], your_position[1])
+	list_rooms = [Room(Room.center,
 		C(dimensions[0], dimensions[1]),
-		C(your_position[0], your_position[1]),
+		host,
 		C(guard_position[0], guard_position[1]),
-		[C(0, 1), C(-1, 0), C(0, -1), C(1, 0)])
+		["N", "S", "W", "E"])]
+	count = 0
+	while len(list_rooms) > 0:
+		new_list = []
+		for room in list_rooms:
+			if host.distance(room.target) <= distance:
+				if not (room.host != host and (host.x == room.target.x or host.y == room.target.y)):
+					count += 1
+				room.clone(new_list)
+		list_rooms = new_list
+	return count
+
+print(str(solution([300,275], [150,150], [185,100], 500)))
